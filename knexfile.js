bf0a5103a3615aa03d16eb1e5529ed9ed6dec88c
unknown <superdracox3@gmail.com>
2024-09-14 17:08:26 -0500

    Add additional env configs

--- knexfile.js
+++ knexfile.js
@@ -19,4 +19,21 @@ module.exports = {
       directory: './seeds'
     }
   },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      port: process.env.PG_PORT,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
 };
