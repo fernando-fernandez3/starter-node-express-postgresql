2a72ee637cbe23ee49f41ab50e031fe115d66b1d
fernando-fernandez3 <50898844+fernando-fernandez3@users.noreply.github.com>
2024-09-14 19:30:42 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const app = require("./app");
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const listener = () => console.log(`Listening on Port ${port}!`);
+app.listen(port, listener);
